dcec3da7b60de93f1d883ee52f617c081680c617
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2019-11-07 15:14:16 -0300

    Finalização da aula com novas function no js, Onclick,onMouseOver, OnMouseOut, OnLoad e OnChange

--- js/main.js
+++ js/main.js
@@ -17,12 +17,29 @@ function divasao(val1, val2) {
 }
 
 function clique() {
-   // alert("O botão foi clicado");
-   document.getElementById("h3-text").innerHTML="Obrigado por clicar";
-   }
+    // alert("O botão foi clicado");
+    document.getElementById("h3-text").innerHTML = "Obrigado por clicar";
+}
 
-   function redir(){
-       window.open("http://buscarlojas.com.br/");//nova aba
-       window.location.href="http://buscarlojas.com.br/";//mesma aba
-   }
-   //9:00
+function redir() {
+    window.open("http://buscarlojas.com.br/");//nova aba
+    window.location.href = "http://buscarlojas.com.br/";//mesma aba
+}
+
+function trocar(e) {
+    // alert("teste");
+    e.innerHTML = "O mouse passou por aqui.";
+}
+
+function voltar(e) {
+    e.innerHTML = "Passe o mouse.";
+}
+
+function Load() {
+    alert("Pagina carregada com sucesso.");
+}
+
+function Change(e) {
+    console.log(e.value);
+
+}
